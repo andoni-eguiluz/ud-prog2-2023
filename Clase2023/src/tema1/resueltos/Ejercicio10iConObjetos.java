d9eb7bee5388d5d3277ec9957855503d32abecc1
Andoni Eguíluz <andoni-eguiluz@users.noreply.github.com>
2023-02-27 14:01:38 +0100

    Resolviendo ejercicio 1.0.i con objetos

---
+++ Clase2023/src/tema1/resueltos/Ejercicio10iConObjetos.java
@@ -0,0 +1,53 @@
+package tema1.resueltos;
+
+public class Ejercicio10iConObjetos {
+	//	Crea dos arrays, uno con los nombres de 4 o 5 usuarios de cualquier red social que uses,
+	//	y otro con sus seguidores. Realiza un programa que muestre por consola esos usuarios 
+	//  primero sin ordenar y luego ordenados por número de seguidores 
+	//	(de mayor a menor).
+
+	public static void main(String[] args) {
+		// String[] usuarios = { "@sama", "@JeffBezos", "@BillGates", "@elonmusk" };
+		// int[] seguidores = { 1300, 61000, 62000, 128900 };
+		
+		/* Manera 1: crear array y luego meter datos
+		UsuarioRedSocial[] usuariosRS = new UsuarioRedSocial[4];
+		usuariosRS[0] = new UsuarioRedSocial();
+		// System.out.println( usuariosRS[0].nombre.substring(0,2) );  NO FUNCIONA - NullPointer
+		usuariosRS[0].nombre = new String("@sama");
+		// System.out.println( usuariosRS[0].nombre.substring(0,2) );
+		usuariosRS[0].seguidores = 1300;
+		// Faltaría 1, 2, 3  */
+		
+		/* Manera 2: crear objetos y luego crear el array */
+		UsuarioRedSocial usuario1 = new UsuarioRedSocial( "@sama", 1300 );
+		// ...
+		UsuarioRedSocial[] usuariosRS = { usuario1  };
+		
+		// new UsuarioRedSocial();  Crea un objeto pero no va a ningún sitio
+		visualizarUsuariosYSeguidores( usuariosRS );
+		ordenaUsuariosPorSeguidores( usuariosRS );
+		visualizarUsuariosYSeguidores( usuariosRS );
+	}
+	
+	// Visualiza línea a línea usuario tabulador nº seguidores
+	private static void visualizarUsuariosYSeguidores(UsuarioRedSocial[] usuariosRS) {
+		for (int i=0; i<usuariosRS.length; i++) {
+			System.out.println( usuariosRS[i].nombre + "\t" + usuariosRS[i].seguidores );
+		}
+	}
+	
+	private static void ordenaUsuariosPorSeguidores(UsuarioRedSocial[] usuariosRS) {
+		for (int pasada=0; pasada<usuariosRS.length-1; pasada++) {
+			for (int comp=0; comp<usuariosRS.length-1; comp++) {  // TODO mejorar que solo se hagan las comparaciones necesarias
+				boolean hayQueIntercambiar = usuariosRS[comp].seguidores < usuariosRS[comp+1].seguidores;
+				if (hayQueIntercambiar) {
+					UsuarioRedSocial aux = usuariosRS[comp];
+					usuariosRS[comp] = usuariosRS[comp+1];
+					usuariosRS[comp+1] = aux;
+				}
+			}
+		}
+	}
+	
+}
---
+++ Clase2023/src/tema1/resueltos/UsuarioRedSocial.java
@@ -0,0 +1,15 @@
+package tema1.resueltos;
+
+public class UsuarioRedSocial {
+	public String nombre;  // COMPOSICIÓN
+	public int seguidores;
+	
+	public UsuarioRedSocial() {
+		
+	}
+	
+	public UsuarioRedSocial( String nombre, int seguidores ) {
+		this.nombre = nombre;
+		this.seguidores = seguidores;
+	}
+}
