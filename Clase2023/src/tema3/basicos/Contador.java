d23d5d9ebcf9244a9e19e00c8e53bc59d564cbde
Andoni Eguíluz <andoni-eguiluz@users.noreply.github.com>
2023-03-23 10:57:53 +0100

    Javadoc de Contador

--- Clase2023/src/tema3/basicos/Contador.java
+++ Clase2023/src/tema3/basicos/Contador.java
@@ -1,5 +1,8 @@
 package tema3.basicos;
 
+/** Clase contador mutable - para el ejemplo de Java Collections (EjemploJC)
+ * @author andoni.eguiluz at ingenieria.deusto.es
+ */
 public class Contador {
 	private int cont = 0;
 	/** Inicializa un contador con valor 1
@@ -8,10 +11,15 @@ public class Contador {
 		cont = 1;
 	}
 	
+	/** Devuelve el valor del contador
+	 * @return	Valor actual
+	 */
 	public int get() {
 		return cont;
 	}
 	
+	/** Incrementa el contador en una unidad
+	 */
 	public void inc() {
 		cont++;
 	}
